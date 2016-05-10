94acca3e4bda844dda578bcc38c51f20c00e96ec
Rafael Moreira <rafadev7@gmail.com>
2016-05-10 02:21:18 -0300

    adding server

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/braintree/manners"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 const tokenEnv string = "TOKEN"
+const portEnv string = "OPENSHIFT_GO_PORT"
 
 type State uint
 
@@ -24,6 +30,11 @@ func main() {
 		log.Panic("TOKEN ENV NOT FOUND!")
 	}
 
+	port := os.Getenv(portEnv)
+	if port == "" {
+		log.Panic("PORT ENV NOT FOUND!")
+	}
+
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
@@ -37,6 +48,27 @@ func main() {
 	news.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(news)
+
+	mux := http.NewServeMux()
+	// Starting a Web Server never idles our instance
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ON!"))
+	})
+	// Shut the server down gracefully
+	processStopedBySignal()
+
+	// Manners allows you to shut your Go webserver down gracefully, without dropping any requests
+	err = manners.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Panic(err)
+		return
+	} else {
+		log.Println("Server start at port :" + port)
+	}
+	defer manners.Close()
+	// END Web Server
 
 	for update := range updates {
 
@@ -60,4 +92,29 @@ func main() {
 
 	}
 
+}
+
+// Shut the server down gracefully if receive a interrupt signal
+func processStopedBySignal() {
+	// Stop server if someone kills the process
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	//signal.Notify(c, syscall.SIGSTOP)
+	signal.Notify(c, syscall.SIGABRT) // ABORT
+	signal.Notify(c, syscall.SIGKILL) // KILL
+	signal.Notify(c, syscall.SIGTERM) // TERMINATION
+	signal.Notify(c, syscall.SIGINT)  // TERMINAL INTERRUPT (Ctrl+C)
+	signal.Notify(c, syscall.SIGSTOP) // STOP
+	signal.Notify(c, syscall.SIGTSTP) // TERMINAL STOP (Ctrl+Z)
+	signal.Notify(c, syscall.SIGQUIT) // QUIT (Ctrl+\)
+	go func() {
+		fmt.Println("THIS PROCESS IS WAITING SIGNAL TO STOP GRACEFULLY")
+		for sig := range c {
+			fmt.Println("\n\nSTOPED BY SIGNAL:", sig.String())
+			fmt.Println("SHUTTING DOWN GRACEFULLY!")
+			fmt.Println("\nGod bye!")
+			manners.Close()
+			os.Exit(1)
+		}
+	}()
 }
