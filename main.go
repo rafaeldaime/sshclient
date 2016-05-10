2666aa78841df88bc2b9adfe47ff422335cca0e7
Rafael Moreira <rafadev7@gmail.com>
2016-05-10 02:55:20 -0300

    adding /status

--- main.go
+++ main.go
@@ -62,6 +62,11 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ON!"))
 	})
+	http.Handle("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<a href=\"https://www.uptimia.com/\" target=\"_blank\"><img src=\"https://www.uptimia.com/status?hash=54bd14d2474753185fb6a66b9239a3f8\" width=\"130\" height=\"auto\" alt=\"Website monitoring | Uptimia\" title=\"Website monitoring | Uptimia\"></a>"))
+	})
 	// Shut the server down gracefully
 	processStopedBySignal()
 
