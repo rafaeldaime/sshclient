48d45fb8acd9fbe0068da920fcf725b02beb66cf
Rafael Moreira <rafadev7@gmail.com>
2016-05-10 03:17:53 -0300

    refactor

--- aux.go
+++ aux.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
+	"github.com/braintree/manners"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 
 	"golang.org/x/crypto/ssh"
@@ -103,4 +110,68 @@ func Run(u *User, cmd string) error {
 	u.Println(string(stdErr))
 
 	return nil
+}
+
+func StartWebServer() {
+
+	ip := os.Getenv(ipEnv)
+	if ip == "" {
+		log.Panic("PORT ENV NOT FOUND!")
+	}
+
+	port := os.Getenv(portEnv)
+	if port == "" {
+		log.Panic("PORT ENV NOT FOUND!")
+	}
+	// WebServer Workaround
+	// These routes are pingged for services just to never idles this instance
+	mux := http.NewServeMux()
+	// Starting a Web Server never idles our instance
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<a href=\"https://www.uptimia.com/\" target=\"_blank\"><img src=\"https://www.uptimia.com/status?hash=54bd14d2474753185fb6a66b9239a3f8\" width=\"130\" height=\"auto\" alt=\"Website monitoring | Uptimia\" title=\"Website monitoring | Uptimia\"></a>"))
+	})
+	// Shut the server down gracefully
+	processStopedBySignal()
+	// Manners allows you to shut your Go webserver down gracefully, without dropping any requests
+	err := manners.ListenAndServe(ip+":"+port, mux)
+	if err != nil {
+		log.Panic(err)
+		return
+	} else {
+		log.Println("Server listening at " + ip + ":" + port)
+	}
+	defer manners.Close()
+	// END of WebServer Workaround
+}
+
+// Shut the server down gracefully if receive a interrupt signal
+func processStopedBySignal() {
+	// Stop server if someone kills the process
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	//signal.Notify(c, syscall.SIGSTOP)
+	signal.Notify(c, syscall.SIGABRT) // ABORT
+	signal.Notify(c, syscall.SIGKILL) // KILL
+	signal.Notify(c, syscall.SIGTERM) // TERMINATION
+	signal.Notify(c, syscall.SIGINT)  // TERMINAL INTERRUPT (Ctrl+C)
+	signal.Notify(c, syscall.SIGSTOP) // STOP
+	signal.Notify(c, syscall.SIGTSTP) // TERMINAL STOP (Ctrl+Z)
+	signal.Notify(c, syscall.SIGQUIT) // QUIT (Ctrl+\)
+	go func() {
+		fmt.Println("THIS PROCESS IS WAITING SIGNAL TO STOP GRACEFULLY")
+		for sig := range c {
+			fmt.Println("\n\nSTOPED BY SIGNAL:", sig.String())
+			fmt.Println("SHUTTING DOWN GRACEFULLY!")
+			fmt.Println("\nGod bye!")
+			manners.Close()
+			os.Exit(1)
+		}
+	}()
 }
--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/braintree/manners"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -31,20 +26,13 @@ func main() {
 		log.Panic("TOKEN ENV NOT FOUND!")
 	}
 
-	ip := os.Getenv(ipEnv)
-	if ip == "" {
-		log.Panic("PORT ENV NOT FOUND!")
-	}
-
-	port := os.Getenv(portEnv)
-	if port == "" {
-		log.Panic("PORT ENV NOT FOUND!")
-	}
-
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
+
+	// Start this webserver just to never puts this instance idle
+	StartWebServer()
 
 	bot.Debug = false
 
@@ -54,32 +42,6 @@ func main() {
 	news.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(news)
-
-	mux := http.NewServeMux()
-	// Starting a Web Server never idles our instance
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("pong"))
-	})
-	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("<a href=\"https://www.uptimia.com/\" target=\"_blank\"><img src=\"https://www.uptimia.com/status?hash=54bd14d2474753185fb6a66b9239a3f8\" width=\"130\" height=\"auto\" alt=\"Website monitoring | Uptimia\" title=\"Website monitoring | Uptimia\"></a>"))
-	})
-	// Shut the server down gracefully
-	processStopedBySignal()
-
-	// Manners allows you to shut your Go webserver down gracefully, without dropping any requests
-	err = manners.ListenAndServe(ip+":"+port, mux)
-	if err != nil {
-		log.Panic(err)
-		return
-	} else {
-		log.Println("Server listening at " + ip + ":" + port)
-	}
-	defer manners.Close()
-	// END Web Server
 
 	for update := range updates {
 
@@ -103,29 +65,4 @@ func main() {
 
 	}
 
-}
-
-// Shut the server down gracefully if receive a interrupt signal
-func processStopedBySignal() {
-	// Stop server if someone kills the process
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	//signal.Notify(c, syscall.SIGSTOP)
-	signal.Notify(c, syscall.SIGABRT) // ABORT
-	signal.Notify(c, syscall.SIGKILL) // KILL
-	signal.Notify(c, syscall.SIGTERM) // TERMINATION
-	signal.Notify(c, syscall.SIGINT)  // TERMINAL INTERRUPT (Ctrl+C)
-	signal.Notify(c, syscall.SIGSTOP) // STOP
-	signal.Notify(c, syscall.SIGTSTP) // TERMINAL STOP (Ctrl+Z)
-	signal.Notify(c, syscall.SIGQUIT) // QUIT (Ctrl+\)
-	go func() {
-		fmt.Println("THIS PROCESS IS WAITING SIGNAL TO STOP GRACEFULLY")
-		for sig := range c {
-			fmt.Println("\n\nSTOPED BY SIGNAL:", sig.String())
-			fmt.Println("SHUTTING DOWN GRACEFULLY!")
-			fmt.Println("\nGod bye!")
-			manners.Close()
-			os.Exit(1)
-		}
-	}()
 }
